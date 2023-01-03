abc5f18f4fe60eadfe688ea4adad8c6b58013117
Allisson Lima <Allysoncosta@outlook.com>
2023-01-03 16:13:50 -0300

    feat: add details pagination table request card

--- src/styles/colorMode.ts
+++ src/styles/colorMode.ts
@@ -8,6 +8,7 @@ export const useColorModeDefault = () => {
   const tab_text = useColorModeValue('#ccc', '#cccccc49');
   const hover_accordion = useColorModeValue('#f8f7faf0', '#373f58');
   const hover_tablet = useColorModeValue('#f8f7faf0', '#282e3f');
+  const header_table = useColorModeValue('#0000000c', '#cbd3e023');
   const expanded_color = useColorModeValue('#edf2f7', '#282e3f');
   const divider_color = useColorModeValue('#cfcfcfff', '#ffffff3e');
   const button_pagination_inative = useColorModeValue('#cfcfcfff', '#8181826b');
@@ -25,5 +26,6 @@ export const useColorModeDefault = () => {
     button_pagination_active,
     expanded_color,
     hover_accordion,
+    header_table,
   };
 };
